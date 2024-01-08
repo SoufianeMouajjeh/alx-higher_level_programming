916bd54a96eae0f8893493ea1a8ffdf118da9125
Soufiane El mouajjeh <137649810+SoufianeMouajjeh@users.noreply.github.com>
2024-01-08 22:01:10 +0000

    done

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Module that import an ractagle"""
+
+BaseGeometry = __import__('9-base_geometry').BaseGeometry
+
+class Square:
+
+    def __init__(self, size):
+        """
+        init method to initialize the Square
+        """
+        self.integer_validator("size", size)
+        self.__size = 0
+        self.__size = size
+        super().__init__(size, size)
+
+    def area(self):
+        """
+        area method to calculate the area of the Square
+        """
+        return self.__size ** 2
+    
