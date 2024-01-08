29ca6d9cb31e04f223a51c31dc36f408ea1ad4d9
Soufiane El mouajjeh <137649810+SoufianeMouajjeh@users.noreply.github.com>
2024-01-08 21:30:37 +0000

    done

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+class BaseGeometry
+"""
+class BaseGeometry:
+    """
+    class BaseGeometry
+    """
+    def area(self):
+        """
+        raise an exception
+        """
+        raise("area() is not implemented")
