8e8e236a8437d9edbeeb4c57d652978fbc1c62cf
Soufiane El mouajjeh <137649810+SoufianeMouajjeh@users.noreply.github.com>
2024-01-09 13:09:46 +0000

    fixed task1

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Module Write a text in file"""
 def write_file(filename="", text=""):
+    """Write a string to a text UTF8"""
     with open(filename, "w", encoding="utf-8",) as file:
         file.write(text),
     return len(text)
