c358a14e663795b91b42b283718bac96d9d06e43
Soufiane El mouajjeh <137649810+SoufianeMouajjeh@users.noreply.github.com>
2024-01-09 14:05:09 +0000

    task3

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+import json
+
+"""Module that return the JSON representation"""
+
+def to_json_string(my_obj):
+    """Retutrn the JSON Represntation"""
+    return json.dumps(my_obj)
