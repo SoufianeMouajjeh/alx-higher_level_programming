c73d3d8404941278b971b57295fd36f1fb5453eb
Soufiane El mouajjeh <137649810+SoufianeMouajjeh@users.noreply.github.com>
2024-03-16 14:47:24 +0000

    Update 4-cities_by_state.py

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,15 +1,20 @@
 #!/usr/bin/python3
-"""Start link class to table in database 
 """
-import sys
+4-cities_by_state module
+"""
 import MySQLdb
+import sys
 
-if __name__ == "__main__":
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3], port=3306, host="localhost")
-    cursor = db.cursor()
-    cursor.execute("SELECT cities.id, cities.name, states.name FROM cities JOIN states ON cities.state_id = states.id ORDER BY cities.id ASC")
-    rows = cursor.fetchall()
+if __name__ == '__main__':
+    db = MySQLdb.connect(host='localhost', port=3306,
+                         user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+
+    cur = db.cursor()
+    cur.execute("""SELECT cities.id, cities.name, states.name FROM
+                cities INNER JOIN states ON states.id=cities.state_id
+                ORDER BY cities.id ASC""")
+    rows = cur.fetchall()
     for row in rows:
         print(row)
-    cursor.close()
+    cur.close()
     db.close()
