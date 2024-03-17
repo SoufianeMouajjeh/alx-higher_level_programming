8f214abdd20aeee57ab88eff1960b5d17f18356a
SoufianeMouajjeh <melmouajjeh@gmail.com>
2024-03-17 14:04:57 +0000

    fix ralationship

--- 0x0F-python-object_relational_mapping/relationship_city.py
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -1,19 +1,17 @@
 #!/usr/bin/python3
-"""
-    Class definition of City
-"""
+"""Definition of the City class"""
+
+from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String, ForeignKey
-from sqlalchemy.orm import declarative_base
 
 Base = declarative_base()
 
 
 class City(Base):
-    """City class inherits from Base"""
+    """Class City"""
+
     __tablename__ = 'cities'
-    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    id = Column(Integer, autoincrement=True,
+                primary_key=True, nullable=False, unique=True)
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
-
-    def __repr__(self):
-        return f"({self.id}) {self.name}"
--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -1,33 +1,18 @@
 #!/usr/bin/python3
-"""
-python file that contains the class definition
-of a State and an instance Base = declarative_base()
-"""
+"""Definition of the State class with relationship to City"""
+
+from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String
-from sqlalchemy.orm import declarative_base, relationship
-from relationship_city import Base
+from sqlalchemy.orm import relationship
+from relationship_city import City, Base
 
 
 class State(Base):
-    """State class inherits from Base"""
+    """Class State"""
+
     __tablename__ = 'states'
-    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    id = Column(Integer, autoincrement=True,
+                primary_key=True, nullable=False, unique=True)
     name = Column(String(128), nullable=False)
-    cities = relationship("City", backref="state", cascade="all, delete")
-
-    def __repr__(self):
-        return f"{self.id}: {self.name}"
-
-
-if __name__ == "__main__":
-    from sqlalchemy import create_engine
-    import sys
-    ur = sys.argv[1]
-    ps = sys.argv[2]
-    db = sys.argv[3]
-    p = 3306
-    pool_pre_ping = True
-    pn = True
-    engine = create_engine(f'mysql+mysqldb://{ur}:{ps}@localhost:{p}/{db}', pn)
-
-    Base.metadata.create_all(engine)
+    cities = relationship('City', backref='state',
+                          cascade='all, delete-orphan')
