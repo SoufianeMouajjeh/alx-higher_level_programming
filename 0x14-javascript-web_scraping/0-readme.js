3fea5d3d7d064b9af7a9dc77b40be326aca12cc3
SoufianeMouajjeh <melmouajjeh@gmail.com>
2024-04-23 22:56:57 +0100

    fix some issue

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -6,11 +6,10 @@ const filePath = process.argv[2];
 
 // Read the file
 fs.readFile(filePath, 'utf8', (err, data) => {
-    if (err) {
-        console.error(err);
-        return;
-    }
-    
-    // Print the file content
-    console.log(data);
+  if (err) {
+    console.error(err);
+    return;
+  }
+
+  console.log(data);
 });
